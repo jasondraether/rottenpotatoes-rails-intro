bad6ecbc037f1ff047550f6b1d1c53618626dac5
jasondraether <jasondraether@gmail.com>
2020-09-15 21:16:25 +0000

    part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    all_ratings = ['G','PG','PG-13','R']
+    @all_ratings = ['G','PG','PG-13','R']
     user_ratings = params[:ratings]
     sort_type = params[:sort_type]
     
